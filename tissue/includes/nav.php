f30be46209685d79dcd3e8d8dab0788c7e4ee390
root <root@plantisc2.sci-gaia.eu>
2017-03-17 17:37:53 +0000

    Fix some minor visualization issues.

--- tissue/includes/nav.php
+++ tissue/includes/nav.php
@@ -3,7 +3,7 @@
 <link href="css/dropdown/themes/default/default.css" media="screen" rel="stylesheet" type="text/css" />
 	<ul id="nav" class="dropdown dropdown-horizontal">
 	<li><a class="" href="/index.php">Home</a> </li>
-	<li><a class="" href="/register.php">Register</a> </li>
+	<!--li><a class="" href="/register.php">Register</a> </li-->
 		<li><a class="" href="/userlogin.php">Login</a> </li>
 		<li><a class="" href="/guide.php">Instructions</a> </li>
 		<li><a class="" href="/temp.php">Template</a> </li>
